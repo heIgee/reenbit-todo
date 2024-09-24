3066627313f86ba6020422c3bc099c1e7c2be940
heIgee <oleo7979@gmail.com>
2024-09-24 14:52:22 +0300

    vite setup

---
+++ client/src/main.tsx
@@ -0,0 +1,10 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from '@/components/App.tsx';
+import '@/styles/main.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
